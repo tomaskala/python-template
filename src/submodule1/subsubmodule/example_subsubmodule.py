a3389a6dfdd4928bc234579e9cf01d52de83facf
Tomas Kala <me@tomaskala.com>
2021-01-09 20:52:41 +0100

    Example project structure

---
+++ src/submodule1/subsubmodule/example_subsubmodule.py
@@ -0,0 +1,2 @@
+def example(callsite):
+    print(f"Example subsubmodule, called from {callsite}")
---
+++ src/submodule2/example_submodule.py
@@ -0,0 +1,6 @@
+from submodule1.subsubmodule.example_subsubmodule import example as sub_example
+
+
+def example():
+    print("Example submodule 2")
+    sub_example("submodule 2")
