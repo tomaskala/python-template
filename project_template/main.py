8ef92b4e10a50edf6ab4abc72f0480f8b03f4889
Tomas Kala <me@tomaskala.com>
2021-01-09 21:56:14 +0100

    Fix imports

--- project_template/main.py
+++ project_template/main.py
@@ -1,6 +1,10 @@
-from submodule1.example_submodule import example as example1
-from submodule1.subsubmodule.example_subsubmodule import example as sub_example
-from submodule2.example_submodule import example as example2
+from project_template.submodule1.example_submodule import example as example1
+from project_template.submodule1.subsubmodule.example_subsubmodule import (
+    example as sub_example,
+)
+from project_template.submodule2.example_submodule import example as example2
+
+__all__ = ["main"]
 
 
 def main():
